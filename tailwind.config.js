459987bd971ac87294c86d6c410d556d81b99494
miroslaw-mroczkowski <mroczkowski.miroslaw@gmail.com>
2023-10-25 14:03:06 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+module.exports = {
+	content: ['./index.html', './src/**/{html,js}'],
+	theme: {
+		extend: {
+			colors: {
+				customTextPrimaryDark: '#1d1e35',
+				customTextPrimaryRed: '#f47c57',
+				customPrimaryViolet: '#af67e9',
+				customPrimaryBlue: '#6565e7',
+				customTextDarkGrayish: '#4a4b5e',
+				customTextGrayish: '#787887',
+				customGrayishLight: '#e7e7e9',
+			},
+			fontFamily: {
+				kumbh: ['Kumbh Sans', ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+	plugins: [],
+};
