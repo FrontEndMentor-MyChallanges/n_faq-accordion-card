50318cc64cdf1739851ada7cc7a51f335341ea32
miroslaw-mroczkowski <mroczkowski.miroslaw@gmail.com>
2023-10-26 12:18:03 +0200

    Mobile design, accordion logic

--- src/script.js
+++ src/script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const items = document.querySelectorAll('.accordion-item');
+const accordion = document.querySelector('.accordion');
+
+let opened = '';
+
+accordion.addEventListener('click', function (e) {
+	e.preventDefault();
+	const clicked = e.target.closest('.accordion-item');
+	if (!clicked) return;
+
+	items.forEach(item => {
+		item.querySelector('.answer').classList.add('hidden');
+		item.querySelector('.question').classList.remove('font-bold');
+		item.querySelector('.arrow').classList.remove('rotate-180');
+	});
+
+	if (!opened || opened !== clicked.dataset.tab) {
+		clicked.querySelector('.answer').classList.remove('hidden');
+		clicked.querySelector('.question').classList.add('font-bold');
+		clicked.querySelector('.arrow').classList.add('rotate-180');
+		opened = clicked.dataset.tab;
+	} else if (opened === clicked.dataset.tab) {
+		clicked.querySelector('.answer').classList.add('hidden');
+		clicked.querySelector('.question').classList.remove('font-bold');
+		clicked.querySelector('.arrow').classList.remove('rotate-180');
+		opened = '';
+	}
+});
